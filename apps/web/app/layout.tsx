a320e8171d9905d9003a36f0e733dd9174f647b4
Oleg <oleg@my-ai-agent.local>
2026-09-19 19:34:07 +0300

    chore: replace application favicon set

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -18,8 +18,8 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       { url: '/favicon.ico', sizes: 'any' },
-      { url: '/favicon-32x32.png', type: 'image/png', sizes: '32x32' },
-      { url: '/favicon-16x16.png', type: 'image/png', sizes: '16x16' },
+      { url: '/favicon.svg', type: 'image/svg+xml' },
+      { url: '/favicon-96x96.png', type: 'image/png', sizes: '96x96' },
     ],
     apple: '/apple-touch-icon.png',
   },
